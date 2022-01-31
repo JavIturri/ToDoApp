9ae91db66809ced7e4691ac9259df202f6fc1b5d
Javier Iturri <javier.iturri@biko2.com>
2022-01-31 13:34:39 +0100

    Añadir y mostrar tareas de un array

--- public/javascript/addTasks.js
+++ public/javascript/addTasks.js
@@ -1,5 +1,46 @@
-/* const div = document.createElement("div");
-div.textContent = "Elemento 1";
-document.getElementById("js").appendChild(div); */
+let arrayTask = [];
+let inputTask = "";
+function getTask(event) {
+  inputTask = event.target.value;
+  arrayTask.push("hola");
+  console.log(arrayTask);
+}
+function addTask(task) {
+  var li = document.createElement("li");
+  var content = document.createTextNode(task);
 
-document.write("hola");
+  li.appendChild(content);
+
+  document.getElementById("list").innerHTML = li.innerHTML;
+}
+
+function printArr(array) {
+  document.getElementById("list").innerHTML = array.innerHTML;
+}
+
+function addTask(array) {
+  array.map((x) => {
+    var p = document.createElement("p");
+    var content = document.createTextNode(x);
+    li.appendChild(content);
+  });
+  var p = document.createElement("p");
+  var content = document.createTextNode(task);
+
+  li.appendChild(content);
+
+  document.getElementById("list").innerHTML = li.innerHTML;
+}
+
+//buena mierda
+
+for (var i = 0; i < arrayTask.length; i++) {
+  var li = document.createElement("p");
+  var content = document.createTextNode(arrayTask[i]);
+
+  li.appendChild(content);
+
+  var list = document.getElementById("tasks");
+
+  list.appendChild(li);
+}
